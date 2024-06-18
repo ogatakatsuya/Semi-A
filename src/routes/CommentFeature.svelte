85cfe4277067ca86245cae37668d27d030f46dfc
09B22019 小田風太 <u346788c@ecs.osaka-u.ac.jp>
2024-06-18 19:34:52 +0900

    LPの2つのサービス機能（「投票結果の表示機能」と「コメント投稿・表示機能」）の追加をしました。

---
+++ src/routes/CommentFeature.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let title = "コメント投稿・表示機能";
+	export let subtitle = "ダイナミックなコメント機能"
+    export let description = "オーディエンスはリアルタイムでコメントを投稿でき、コメントは一定時間表示された後に消えます。Svelteの反応性の高いフレームワークにより、コメントが投稿されるたびに即座に画面に反映されます。さらに、Svelteのスマートなスタイル適用機能を利用して、投稿が多いコメントは自動的に大きく表示され、重要なフィードバックが一目でわかるようになります。これにより、オーディエンスの声を的確に拾い上げることが可能です。";
+</script>
+
+<section class="feature">
+	<h3>{title}</h3>
+    <h4>{subtitle}</h4>
+	<p>{description}</p>
+</section>
+
+<style>
+	.feature {
+		margin-bottom: 1.5rem;
+	}
+
+	.feature h3 {
+		font-size: 1.75rem;
+		color: #0056b3;
+	}
+
+    .feature h4 {
+		font-size: 1.25rem;
+		color: #666;
+	}
+
+	.feature p {
+		font-size: 1rem;
+		color: #666;
+	}
+</style>
---
+++ src/routes/ResultFeature.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let title = "投票結果の表示機能";
+    export let subtitle = "インタラクティブな投票結果表示"
+	export let description = "投票結果は、Svelteの強力なデータバインディングとアニメーション機能を活用して、リアルタイムで円グラフとして視覚化されます。これにより、オーディエンスは結果を一目で理解でき、プレゼンテーションの効果が一層高まります。Svelteの再レンダリングの速さと効率性により、投票結果の表示はスムーズでシームレスな体験を提供します。";
+</script>
+
+<section class="feature">
+	<h3>{title}</h3>
+    <h4>{subtitle}</h4>
+	<p>{description}</p>
+</section>
+
+<style>
+	.feature {
+		margin-bottom: 1.5rem;
+	}
+
+	.feature h3 {
+		font-size: 1.75rem;
+		color: #0056b3;
+	}
+
+    .feature h4 {
+		font-size: 1.25rem;
+		color: #666;
+	}
+
+	.feature p {
+		font-size: 1rem;
+		color: #666;
+	}
+</style>
