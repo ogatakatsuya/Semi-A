c94e3f468629c16ed7e51aa9ccf0a338f650673c
“09B22021” <09B22021@localhost>
2024-06-19 23:10:02 +0900

    header変更

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -13,6 +13,60 @@
 		</a>
 	</div>
 
+	//以下は変更箇所
+	<div class="accordion-container">
+		<Accordion>
+			<Panel>
+			<Header>Panel 1</Header>
+			<Content>
+				The content for panel 1.
+		
+				<ul>
+				<li>Some</li>
+				<li>List</li>
+				<li>Items</li>
+				</ul>
+			</Content>
+			</Panel>
+			<Panel>
+			<Header>Panel 2</Header>
+			<Content>The content for panel 2.</Content>
+			</Panel>
+			<Panel>
+			<Header>Panel 3</Header>
+			<Content>
+				The content for panel 3.
+		
+				<ul>
+				<li>Some</li>
+				<li>More</li>
+				<li>List</li>
+				<li>Items</li>
+				<li>To</li>
+				<li>Show</li>
+				<li>Big</li>
+				<li>Height</li>
+				</ul>
+			</Content>
+			</Panel>
+			<Panel>
+			<Header>Panel 4</Header>
+			<Content>
+				If you like this component, you can thank <a
+				href="https://github.com/NickantX"
+				target="_blank">nick</a
+				> who pushed me to make it. He was right, accordions are awesome! :D
+			</Content>
+			</Panel>
+		</Accordion>
+		</div>
+	  
+	<script lang="ts">
+		import Accordion, { Panel, Header, Content } from '@smui-extra/accordion';
+	</script>
+	  
+	//変更箇所ここまで
+
 	<nav>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
