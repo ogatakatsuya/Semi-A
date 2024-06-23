ce37100d75deacf73256f1c19098a5fc332e672b
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-06-23 23:42:11 +0900

    Revert "コメント表示機能の実装"

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,8 @@
 export default {
   content: [ "./src/**/*.{js,ts,svelte}", "./src/app.html"],
   theme: {
-    extend: {
-      fontFamily: {
-        noto: ["Noto Sans JP"],
-      }
-    },
+    extend: {},
   },
-  plugins: [
-    function({ addUtilities }) {
-      const newUtilities = {
-        ".text-shadow-flowing": {
-          textShadow: "1px  1px 4px white, -1px  1px 4px white, 1px -1px 4px white, -1px -1px 4px white"
-        },
-        ".text-shadow-none": {
-          textShadow: "none"
-        }
-      };
-      addUtilities(newUtilities);
-    }
-  ],
+  plugins: [],
 }
 
