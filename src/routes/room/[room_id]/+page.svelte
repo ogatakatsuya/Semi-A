cc005f6a5fb364b81946ad1a31551b469450b644
09B22067 <09B22067@localhost>
2024-07-02 10:51:30 +0900

    LocalStorageの実装

--- src/routes/room/[room_id]/+page.svelte
+++ src/routes/room/[room_id]/+page.svelte
@@ -14,6 +14,7 @@
 	import { validateSchema, validator } from '@felte/validator-yup';
 	import * as yup from 'yup';
     import { tick } from "svelte";
+	import { browser } from "$app/environment";
 
 	let text: string = "";
 	let time: Date;
@@ -80,14 +81,37 @@
 					open: data.open,
                 };
 				const id:string = doc.id;
-				if(!(id in message)) message[id] = " ";
-				if(!(id in chosen)) chosen[id] = -1;
-				if(!(id in disabled)) disabled[id] = "";
-				if(!(id in hidden)) hidden[id] = "block";
+				if(!(id in message)) {
+					if((browser) && (localStorage.hasOwnProperty(id)) && (Number(localStorage.getItem(id)) != -1)) {
+						chosen[id] = Number(localStorage.getItem(id));
+						Disable(id);
+					} else {
+						message[id] = "";
+						chosen[id] = -1;
+						disabled[id] = false;
+						hidden[id] = false;
+						if(browser) localStorage.setItem(id, '-1');
+					}
+				} else {
+					if((browser) && (localStorage.hasOwnProperty(id)) && (Number(localStorage.getItem(id)) != -1)) {
+						chosen[id] = Number(localStorage.getItem(id));
+						Disable(id);
+					}
+				}
                 return item;
             });
         }
     );
+	const Disable = (question_id: string) => {
+		disabled[question_id] = true;
+		hidden[question_id] = true;
+		message[question_id] = "投票完了";
+	}
+	const Enable = (question_id: string) => {
+		disabled[question_id] = false;
+		hidden[question_id] = false;
+		message[question_id] = "";
+	}
 	const vote = (question_id: string, q_index: number) => {
 		let chosenIndex = chosen[question_id];
 		if (chosenIndex === -1) {
@@ -99,12 +123,24 @@
 			renew[chosenIndex] = ++cnt;
 			try {
 				const docRef = updateDoc(doc(db, `Rooms/${room_id}/Questions`, `${question_id}`), {"results": renew});
-				disabled[question_id] = "disabled";
-				hidden[question_id] = "hidden";
-				message[question_id] = "投票完了";
+				Disable(question_id);
+				if(browser) localStorage.setItem(question_id, chosen[question_id]);
 			} catch (e) {
-				console.error("Error adding document: ", e);
+				console.error("Error updating document: ", e);
 			}
+		}
+	}
+	const changevote = (question_id: string, q_index: number) => {
+		let chosenIndex = chosen[question_id];
+		let cnt = questions[q_index]["results"][chosenIndex];
+		let renew = questions[q_index]["results"];
+		renew[chosenIndex] = --cnt;
+		try {
+			if(browser) localStorage.setItem(question_id, '-1');
+			Enable(question_id);
+			const docRef = updateDoc(doc(db, `Rooms/${room_id}/Questions`, `${question_id}`), {"results": renew});
+		} catch (e) {
+			console.error("Error updating document: ", e);
 		}
 	}
 </script>
@@ -132,16 +168,26 @@
 				<div class="px-6 pb-3 text-secondary-500">
 				{#each question.options as option, index}
 					<div class="flex items-center space-x-2 rounded p-2 pl-6 hover:bg-gray-100 font-noto">
-						<input type="radio" name="{question.id}" id="{question.id}_{index}" value={index} bind:group={chosen[question.id]} disabled={disabled[question.id]} class="h-4 w-4 rounded-full border-gray-300 text-primary-600 shadow-sm focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 focus:ring-offset-0 disabled:cursor-not-allowed disabled:text-gray-400">
+						<input type="radio" name="{question.id}" id="{question.id}_{index}" value={index} bind:group={chosen[question.id]} disabled={disabled[question.id]} 
+							class="h-4 w-4 rounded-full border-gray-300 text-primary-600 focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 focus:ring-offset-0 disabled:cursor-not-allowed disabled:text-gray-400"
+							>
 						<label for="{question.id}_{index}" class="flex w-full space-x-2 text-sm"> {option} </label>
 					</div>
 				{/each}
-				<div class="text-sm text-red-600 font-noto" style="padding: 10px 1rem; visibility: {message[question.id] ? "visible" : "hidden"}">{message[question.id]}</div>
+				<div class="text-sm text-red-600 font-noto mb-1" style="padding: 10px 1rem; visibility: {message[question.id] ? "visible" : "hidden"}">{message[question.id]}</div>
+				{#if !hidden[question.id]}
 				<button on:click={() => vote(question.id, q_index)}
-					class="{hidden[question.id]} mb-4 block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+					class="mb-4 block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 				>
 					Vote
 				</button>
+				{:else}
+				<button on:click={() => changevote(question.id, q_index)}
+					class="mb-4 block text-white bg-gray-500 hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-gray-400 dark:hover:bg-gray-500 dark:focus:ring-gray-600"
+				>
+					Change
+				</button>
+				{/if}
 				</div>
 			</details>
 			{/if}
@@ -151,10 +197,14 @@
 
 	<h2 class="mt-10 mb-4 font-bold text-center">Send your comment!</h2>
 	<form use:form on:submit|preventDefault = {addComment} class="max-w-md mx-auto">
-		<div class="relative">
+		<div class="relative flex items-center">
 			<input bind:value={text} type="comment" name="comment" id="comment" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Input comment" />
-			<button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Submit</button>
-			<div class="text-sm text-red-600" style="padding-bottom: 10px; visibility: {$errors.comment ? "visible" : "hidden"}">{$errors.comment}</div>
+			<button type="button" class="inline-flex items-center justify-center rounded-full h-12 p-3 ml-2 text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-6 w-6 transform rotate-90">
+					<path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+				</svg>
+			</button>
 		</div>
+		<div class="text-sm text-red-600" style="padding-bottom: 10px; visibility: {$errors.comment ? "visible" : "hidden"}">{$errors.comment}</div>
 	</form>
 </div>
